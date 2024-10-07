eeb86737c30ffc15d54fc05eb05afaaa47f80aa9
unorderlyhamster329 <mattng08@gmail.com>
2024-10-07 11:10:22 -0700

    sbd

--- Main.java
+++ Main.java
@@ -2,6 +2,25 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write your code here
+    
+  }
+
+  public static String lastFirstN (String s1, String s2, int n) {
+    int indexOfNthToLast = s1.length() - n;
+    String last = s1.substring(indexOfNthToLast);
+    String first = s2.substring(0, n);
+
+    return last + first;
+  }
+
+  public static void StringManip(final String s1, final String s2) {
+    System.out.println(s1.toUpperCase());
+
+    String firstLetter = s2.substring(0,1);
+    firstLetter = firstLetter.toUpperCase();
+
+    String lower = s2.toLowerCase();
+    String restOfs2 = s2.substring(1);
+    System.out.println(firstLetter + restOfs2);
   }
 }
